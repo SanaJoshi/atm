b6389fe0936a840511385e326fa870b314c8383c
Sana Joshi <sanajoshi@Johns-MacBook-1.local>
2017-08-07 09:35:00 +1000

    first atm

---
+++ atm.rb
@@ -0,0 +1,38 @@
+
+print "Welcome to coderbank"
+
+# pseudocode
+
+# step 1 - tell the user what their options are
+#          - option 1 - deposit
+#          - option 2 - withdraw
+puts "what transcation would like to carry out?
+withdraw or deposite"
+
+transcation= gets.chomp
+
+if transcation == "withdraw"
+  puts "thank you for withdrawing $"
+
+elsif transcation =="deposite"
+  puts "thank you for depositing $"
+
+end 
+#
+# deposit = gets.chomp
+#  withdraw = gets.chomp
+#
+# # step 2 - to be confirmed
+# if operation == "+"
+#   def add(num1, num2)
+#     sum = num1 + num2
+#     puts "#{num1} + #{num2} = #{sum}"
+#   end
+#   add(num1, num2)
+# elsif operation == "-"
+#   def subtract(num1, num2)
+#     sub = num1 - num2
+#     puts "#{num1} - #{num2} = #{sub}"
+#   end
+#   subtract(num1, num2)
+# elsif operation == "*"
